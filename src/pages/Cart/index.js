b8130513c10141c7dc7fa0bf9f73edd4a3d110e2
Rafael Guimarães <rafarodgui@hotmail.com>
2020-01-09 18:19:47 -0300

    agora o produto nao pode mais ser duplicado no carrinho, e pode ser escluido, funcionalidade desenvolvida com o IMMER

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,72 +2,61 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
-import { formatPrice } from '../../util/format';
-
 import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ itens }){
-
-    const data = itens.map( item => ({
-        ...item,
-        price: formatPrice(item.price)
-    }));
-
+function Cart({ itens, dispatch }){
     return(
         <Container>
             <ProductTable>
-                {
-                    data.map(item => (
-                        <>
-                            <thead key={item.id}>
-                            <tr>
-                                <th />
-                                <th>PRODUTO</th>
-                                <th>QTD</th>
-                                <th>SUBTOTAL</th>
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img 
-                                        src={item.image} 
-                                        alt={item.title}
-                                    />
-                                </td>
-                                <td>
-                                    <strong>{item.title}</strong>
-                                    <span>{item.price}</span>
-                                </td>
-                                <td>
-                                    <div>
-                                        <button type="button">
-                                            <MdRemoveCircleOutline size={20} color="#7159c1" />
-                                        </button>
+                <thead>
+                <tr>
+                    <th />
+                    <th>PRODUTO</th>
+                    <th>QTD</th>
+                    <th>SUBTOTAL</th>
+                    <th />
+                </tr>
+            </thead>
+            <tbody>
+                { itens.map(item => (
+                    <tr key={item.id}>
+                        <td>
+                            <img 
+                                src={item.image} 
+                                alt={item.title}
+                            />
+                        </td>
+                        <td>
+                            <strong>{item.title}</strong>
+                            <span>{item.priceFormatted}</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                </button>
 
-                                        <input type="number" readOnly value={1} />
-                                        
-                                        <button type="button">
-                                            <MdAddCircleOutline size={20} color="#7159c1" />
-                                        </button>
-                                    </div>
-                                </td>
-                                <td>
-                                    <strong>R$258,80</strong>
-                                </td>
-                                <td>
-                                    <button type="button">
-                                        <MdDelete size={20} color="#7519c1"/>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </>
-                    ))
-                }
+                                <input type="number" readOnly value={item.amount} />
+                                
+                                <button type="button">
+                                    <MdAddCircleOutline size={20} color="#7159c1" />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button" onClick={() => dispatch({ type: 'REMOVE_ITEM', id: item.id })}>
+                                <MdDelete size={20} color="#7519c1"/>
+                            </button>
+                        </td>
+                    </tr>
+                ))
+            }
+            </tbody>
             </ProductTable>
 
             <footer>
